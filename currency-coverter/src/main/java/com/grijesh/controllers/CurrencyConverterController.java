050bc7bf6f3bc429afbf0a6774370f68662a0d27
grijesh <grijesh.rajoria@gmail.com>
2015-10-31 21:26:00 +0530

    Controllers and Service

--- currency-coverter/src/main/java/com/grijesh/controllers/CurrencyConverterController.java
+++ currency-coverter/src/main/java/com/grijesh/controllers/CurrencyConverterController.java
@@ -1,21 +1,24 @@
 package com.grijesh.controllers;
 
-import com.grijesh.dto.ConvertRequest;
 import com.grijesh.dto.ConvertResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by grijesh on 31/10/15.
+ *
  */
 
 @RestController
 public class CurrencyConverterController {
 
-    @RequestMapping("/currency")
-    public ConvertResponse convertCurrency(@RequestParam ConvertRequest request){
-    return null;
+
+
+    @RequestMapping(value = "/currency",method = RequestMethod.POST)
+    public ConvertResponse convertCurrency(@RequestParam(value = "amount") String amount,@RequestParam(value = "baseCurrency")String baseCurrency,@RequestParam(value = "requestedCurrency") String requestedCurrency){
+     return null;
     }
 
 }
---
+++ currency-coverter/src/main/java/com/grijesh/service/ConvertCurrencyImpl.java
@@ -0,0 +1,14 @@
+package com.grijesh.service;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by grijesh on 31/10/15.
+ */
+public class ConvertCurrencyImpl implements ConvertCurrency {
+
+    @Override
+    public BigDecimal convertCurrency(ConvertCurrency convertCurrency) {
+        return null;
+    }
+}
