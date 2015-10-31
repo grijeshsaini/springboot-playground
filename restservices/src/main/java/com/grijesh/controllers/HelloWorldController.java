851ddd497db156a08e18ff0ff0c69123bbce3581
grijesh <grijesh.rajoria@gmail.com>
2015-10-31 13:35:40 +0530

    Hello world controller !

---
+++ restservices/src/main/java/com/grijesh/controllers/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.grijesh.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by grijesh on 31/10/15.
+ */
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
---
+++ restservices/src/main/java/com/grijesh/main/Application.java
@@ -0,0 +1,19 @@
+package com.grijesh.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by grijesh on 31/10/15.
+ */
+
+@EnableAutoConfiguration
+@ComponentScan("com.grijesh.controllers")
+public class Application{
+
+    public static void main(String ...args){
+        SpringApplication.run(Application.class,args);
+
+    }
+}
