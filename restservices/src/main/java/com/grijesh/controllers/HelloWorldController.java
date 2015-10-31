9c25b20d6170e960a311f8e5747d5976a3331f47
grijesh <grijesh.rajoria@gmail.com>
2015-10-31 18:09:08 +0530

    Rest Service - Default example

--- restservices/src/main/java/com/grijesh/controllers/HelloWorldController.java
+++ restservices/src/main/java/com/grijesh/controllers/HelloWorldController.java
@@ -6,11 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by grijesh on 31/10/15.
  */
+
 @RestController
 public class HelloWorldController {
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Spring Boot!!";
     }
 
 }
